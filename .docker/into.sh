f3218f6d66eb58d655ee32dc7073f1a226f7967b
The Dirk Knight <dirkschnabel03@aol.com>
2022-01-03 21:50:32 +0000

    Prepare docker for gtk apps and enable gui apps to run in docker

--- .docker/into.sh
+++ .docker/into.sh
@@ -3,6 +3,9 @@
 source $(dirname "${BASH_SOURCE[0]}")/include.sh
 
 docker run -it --rm --name coding_sandbox_docker \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    --net=host \
+    --env="DISPLAY" \
     -v `pwd`:/ws/ \
     $dockername bash
 
--- .docker/run_docker.sh
+++ .docker/run_docker.sh
@@ -3,6 +3,9 @@
 source $(dirname "${BASH_SOURCE[0]}")/include.sh
 
 docker run -i --rm --name coding_sandbox_docker \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    --net=host \
+    --env="DISPLAY" \
     -v `pwd`:/ws/ \
     $dockername /bin/bash -c "$@"
 

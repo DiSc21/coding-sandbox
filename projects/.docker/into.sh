cbe024efc88d7c2936d4bf92b9a3eafc90b4e089
The Dirk Knight <dirkschnabel03@aol.com>
2023-01-06 21:57:58 +0000

    Add jenkins build-tag env to docker name for local and jenkins not working on same container

--- projects/.docker/into.sh
+++ projects/.docker/into.sh
@@ -5,7 +5,7 @@ abs_base_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")/../"; pwd)
 
 source $(dirname "${BASH_SOURCE[0]}")/include.sh
 
-docker run -it --rm --name coding_sandbox_docker \
+docker run -it --rm --name ${name} \
     --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
     --volume /etc/passwd:/etc/passwd:ro \
     --volume /etc/group:/etc/group:ro \

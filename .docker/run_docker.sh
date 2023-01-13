adb10e56251799addf50c2ad3c5d6183f2a32ccd
The Dirk Knight <dirkschnabel03@aol.com>
2023-01-13 13:46:17 +0000

    Only rebuild docker image if container not already up and running

--- .docker/run_docker.sh
+++ .docker/run_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source $(dirname "${BASH_SOURCE[0]}")/build_docker.sh
+source $(dirname "${BASH_SOURCE[0]}")/config.sh
 
 if ! [[ $(docker ps | grep ${name}) ]]
 then
+    source $(dirname "${BASH_SOURCE[0]}")/build_docker.sh
     docker run -i --rm --name ${name} \
         --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
         --volume /etc/passwd:/etc/passwd:ro \

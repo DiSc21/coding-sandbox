9da22d8b82ca48a7128347c0c75f0966d0c29135
DiSc21 <59342112+DiSc21@users.noreply.github.com>
2023-01-14 16:35:11 +0000

    Feature/update jenkinsfile n add options (#6)
    * Hopefully fix start/run_docker bug
    
    * Skip docker build if container already up and running
    
    * hm, not sure
    
    * Only rebuild docker image if container not already up and running
    
    * Add function that should trigger some compiler and static code analysis errors/warnings
    
    * Add info to flawfinder results
    
    * Add simple Jenkins option to switch between different build types
    
    * Move test_cpp_code_analysis directory out of projects/cpp
    
    * Try Jenkins workspace cleanup

--- .docker/run_gcovr_on_subprojects.sh
+++ .docker/run_gcovr_on_subprojects.sh
@@ -3,8 +3,12 @@
 source $(dirname "${BASH_SOURCE[0]}")/config.sh
 
 result_dir=$abs_root_dir"/projects/.results"
-if [ $# -eq 0 ]; then
+if ! [ $# -eq 1 ]; then
   echo "Pass at least one existing directory"
+  echo "Exit!"
+  exit 128
+elif ! [ -d "$abs_root_dir/$@" ]; then
+  echo "No directory "$abs_root_dir"/"$@" found."
   echo "Exit!"
   exit 128
 fi
@@ -12,7 +16,6 @@ fi
 csv_make_dirs=$(cd $abs_root_dir/.docker; ./find_make_directories.sh $@)
 
 gcovr_filter_string=$(echo "$csv_make_dirs" | sed -r 's/[,]+/ --filter /g')
-
 gcovr_exclude_string=$(echo "$csv_make_dirs" | sed -r 's/[,]+/\/test --exclude /g')
 gcovr_exclude_string=$gcovr_exclude_string"/test"
 
@@ -21,6 +24,6 @@ gcovr_exclude_flags="--exclude-throw-branches --exclude-unreachable-branches"
 gcovr_flags="--filter ${gcovr_filter_string} --exclude ${gcovr_exclude_string} ${gcovr_exclude_flags}"
 
 ${run_cmd} "if ! [ -d "${result_dir}" ]; then mkdir ${result_dir}; fi"
-${run_cmd} "cd ${abs_root_dir}; gcovr -r . ${gcovr_flags} --html --html-details -o ${result_dir}/gcovr_coverage.html"
-${run_cmd} "cd ${abs_root_dir}; gcovr -r . ${gcovr_flags} --xml -o ${result_dir}/gcovr_coverage.xml"
+${run_cmd} "cd ${abs_root_dir}; gcovr -r $@ ${gcovr_flags} -v --html --html-details -o ${result_dir}/gcovr_coverage.html"
+${run_cmd} "cd ${abs_root_dir}; gcovr -r $@ ${gcovr_flags} -v --xml -o ${result_dir}/gcovr_coverage.xml"
 
